abb7fc596213fe00a03b9a27e5bfad99db29ce99
zhekeani <zhekenai@gmail.com>
2024-03-13 21:25:55 +0700

    simplified forRootAsync in StorageModule

--- libs/common/src/storage/storage.module.ts
+++ libs/common/src/storage/storage.module.ts
@@ -3,12 +3,12 @@ import { StorageService } from './storage.service';
 import { StorageModuleConfig } from './interfaces/storage-module-config.interface';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({
-  providers: [StorageService],
-  exports: [StorageService],
-})
+@Module({})
 export class StorageModule {
+  // method to return module with the configuration provided
+  // by the caller
   static forRootAsync(options: {
+    // Create flexibility to directly return the value or the promise
     useFactory: (
       ...args: any[]
     ) => Promise<StorageModuleConfig> | StorageModuleConfig;
@@ -16,13 +16,18 @@ export class StorageModule {
   }): DynamicModule {
     return {
       module: StorageModule,
-      imports: [ConfigModule.forRoot()],
       providers: [
         StorageService,
         {
           provide: 'CONFIG',
+
+          // Take all providers from inject and pass it to the options factory function
+          // Use async await to accommodate if the return value is a promise
           useFactory: async (...args: any[]) =>
             await options.useFactory(...args),
+
+          // All providers in the inject will be resolved and passed as arguments
+          // to factory function
           inject: options.inject || [ConfigService],
         },
       ],
