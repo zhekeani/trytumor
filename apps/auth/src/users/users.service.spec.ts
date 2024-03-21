2044da9d856a5612dad1fbd0675a3f63b0631c19
zhekeani <zhekenai@gmail.com>
2024-03-21 16:25:04 +0700

    adding Auth micro-service events service test suite

--- apps/auth/src/users/users.service.spec.ts
+++ apps/auth/src/users/users.service.spec.ts
@@ -1,8 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { StorageService } from '@app/common';
+import { EventsService } from '../events/events.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let mockUsersRepository: Partial<UsersRepository>;
+  let mockStorageService: StorageService;
+  let eventsService: Partial<EventsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
